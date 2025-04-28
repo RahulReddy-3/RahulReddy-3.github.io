cf37645837bd1580d2cd3c4be0a0fd71d21d8564
Rahul Reddy Rayapuram <iamrahulreddy.3@gmail.com>
2025-04-27 23:57:07 -0700

    Updated Repo

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,12 +1,1 @@
-// Dark/Light Mode Toggle
-const themeToggle = document.getElementById('themeToggle');
-const body = document.body;
-
-themeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark');
-    if (body.classList.contains('dark')) {
-        themeToggle.textContent = "☀️"; // Light icon
-    } else {
-        themeToggle.textContent = "🌙"; // Dark icon
-    }
-});
+<!-- scripts/darkmode.js will contain the real code -->
